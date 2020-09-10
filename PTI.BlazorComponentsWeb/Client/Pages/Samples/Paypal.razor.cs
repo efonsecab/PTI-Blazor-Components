01c9c08d1eaf3520e160a01ae12e597e85264e7f
eduardofonseca <eduardo.fonseca@pticostarica.com>
2020-09-10 14:24:24 -0600

    Adding missing files

---
+++ PTI.BlazorComponentsWeb/Client/Pages/Samples/Paypal.razor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using PTI.BlazorComponentsWeb.Shared.Paypal;
+
+namespace PTI.BlazorComponentsWeb.Client.Pages.Samples
+{
+    public partial class Paypal
+    {
+        [Inject]
+        private HttpClient httpClient { get; set; }
+        private static HttpClient StaticHttpClient { get; set; }
+
+        protected override void OnInitialized()
+        {
+            StaticHttpClient = this.httpClient;
+        }
+
+        [JSInvokable()]
+        public static async Task OnApproved(OnApprovedData data, object actions)
+        {
+            await StaticHttpClient.PostAsJsonAsync<OnApprovedData>("Paypal/UserSubscribed", data);
+        }
+    }
+}
---
+++ PTI.BlazorComponentsWeb/Server/Controllers/PaypalController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PTI.BlazorComponentsWeb.Shared.Paypal;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PTI.BlazorComponentsWeb.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PaypalController : ControllerBase
+    {
+        [HttpPost("[action]")]
+        public async Task<IActionResult> UserSubscribed(OnApprovedData model)
+        {
+            return Ok();
+        }
+    }
+}
